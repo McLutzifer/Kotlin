6cf36cf439ac710649e3a8c4a50f5c84e3dbd64c
McLutzifer <lukasschumi@gmx.at>
2021-02-14 19:58:54 +0100

    update code with readLine

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,9 @@ fun main(args: Array<String>) {
     println("Hello World!")
 
     var num: Int = 42
-
     println(num)
+
+    println("What's your name?")
+    val name = readLine()
+    println("Hello $name")
 }
